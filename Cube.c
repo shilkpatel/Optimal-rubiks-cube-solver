5e1bb48ba9a3d5f9a75ada116fa1f57c96c8cef4
Shil16 <shilkpatel2004@gmail.com>
2023-06-26 21:14:12 +0100

    added prime moves now need to do double moves

--- Cube.c
+++ Cube.c
@@ -74,6 +74,32 @@ void R(Cube* cube_state)
     free(temp);
 }
 
+void R_prime(Cube* cube_state)
+{
+    cubie* temp = malloc(sizeof(cubie));
+    // shifting corners
+
+    //possible memory leak as memory is allocated but not deallocated
+    temp = &cube_state->corners[0];
+    cube_state->corners[0]=cube_state->corners[4];
+    cube_state->corners[4]=cube_state->corners[5];
+    cube_state->corners[5]=cube_state->corners[1];
+    cube_state->corners[1]=*temp;
+
+    update_corner_orientation(&cube_state->corners[0],2);
+    update_corner_orientation(&cube_state->corners[1],1);
+    update_corner_orientation(&cube_state->corners[5],2);
+    update_corner_orientation(&cube_state->corners[4],1);
+
+    temp = &cube_state->edges[1];
+    cube_state->edges[1]=cube_state->edges[4];
+    cube_state->edges[4]=cube_state->edges[9];
+    cube_state->edges[9]=cube_state->edges[5];
+    cube_state->edges[5]=*temp;
+    free(temp);
+}
+
+
 void L(Cube* cube_state)
 {
     cubie* temp = malloc(sizeof(cubie));
@@ -93,10 +119,30 @@ void L(Cube* cube_state)
     cube_state->edges[7]=cube_state->edges[11];
     cube_state->edges[11]=cube_state->edges[6];
     cube_state->edges[6]=*temp;
+    free(temp);
 
 }
-//0-FUR,1-FDR,2-FDL,3-FUL,4-BUR,5-BDR,6-BDL,7-BUL
-//0-FU,1-FR,2-FD,3-FL,4-UR,5-DR,6-DL,7-UL,8-BU,9-BR,10-BD,11-BL
+
+void L_prime(Cube* cube_state)
+{
+     cubie* temp = malloc(sizeof(cubie));
+    temp = &cube_state->corners[3];
+    cube_state->corners[3]=cube_state->corners[2];
+    cube_state->corners[2]=cube_state->corners[6];
+    cube_state->corners[6]=cube_state->corners[7];
+    cube_state->corners[7]=*temp;
+
+    update_corner_orientation(&cube_state->corners[3],1);
+    update_corner_orientation(&cube_state->corners[7],2);
+    update_corner_orientation(&cube_state->corners[6],1);
+    update_corner_orientation(&cube_state->corners[2],2);
+
+    temp=&cube_state->edges[3];
+    cube_state->edges[3]=cube_state->edges[6];
+    cube_state->edges[6]=cube_state->edges[11];
+    cube_state->edges[11]=cube_state->edges[7];
+    cube_state->edges[7]=*temp;
+}
 
 void F(Cube* cube_state)
 {
@@ -118,6 +164,35 @@ void F(Cube* cube_state)
     cube_state->edges[3]=cube_state->edges[2];
     cube_state->edges[2]=cube_state->edges[1];
     cube_state->edges[1]=*temp;
+    free(temp);
+
+    update_edge_orientation(&cube_state->edges[0],1);
+    update_edge_orientation(&cube_state->edges[3],1);
+    update_edge_orientation(&cube_state->edges[2],1);
+    update_edge_orientation(&cube_state->edges[1],1);
+
+}
+
+void F_prime(Cube* cube_state)
+{
+    cubie* temp = malloc(sizeof(cubie));
+
+    temp =&cube_state->corners[3];
+    cube_state->corners[3]=cube_state->corners[0];
+    cube_state->corners[0]=cube_state->corners[1];
+    cube_state->corners[1]=cube_state->corners[2];
+    cube_state->corners[2]=*temp;
+
+    update_corner_orientation(&cube_state->corners[3],2);
+    update_corner_orientation(&cube_state->corners[2],1);
+    update_corner_orientation(&cube_state->corners[1],2);
+    update_corner_orientation(&cube_state->corners[0],1);
+
+    temp = &cube_state->edges[0];
+    cube_state->edges[0]=cube_state->edges[1];
+    cube_state->edges[1]=cube_state->edges[2];
+    cube_state->edges[2]=cube_state->edges[3];
+    cube_state->edges[3]=*temp;
     free(temp);
 
     update_edge_orientation(&cube_state->edges[0],1);
@@ -155,6 +230,34 @@ void B(Cube* cube_state)
     update_edge_orientation(&cube_state->edges[11],1);
 }
 
+void B_prime(Cube* cube_state)
+{
+    cubie* temp = malloc(sizeof(cubie));
+
+    temp = &cube_state->corners[4];
+    cube_state->corners[4]=cube_state->corners[7];
+    cube_state->corners[7]=cube_state->corners[6];
+    cube_state->corners[6]=cube_state->corners[5];
+    cube_state->corners[5]=*temp;
+
+    update_corner_orientation(&cube_state->corners[4],2);
+    update_corner_orientation(&cube_state->corners[5],1);
+    update_corner_orientation(&cube_state->corners[6],2);
+    update_corner_orientation(&cube_state->corners[7],1);
+
+    temp = &cube_state->edges[8];
+    cube_state->edges[8]=cube_state->edges[11];
+    cube_state->edges[11]=cube_state->edges[10];
+    cube_state->edges[10]=cube_state->edges[9];
+    cube_state->edges[9]=*temp;
+    free(temp);
+
+    update_edge_orientation(&cube_state->edges[8],1);
+    update_edge_orientation(&cube_state->edges[9],1);
+    update_edge_orientation(&cube_state->edges[10],1);
+    update_edge_orientation(&cube_state->edges[11],1);
+}
+
 void U(Cube* cube_state)
 {
     cubie* temp = malloc(sizeof(cubie));
@@ -177,6 +280,26 @@ void U(Cube* cube_state)
     //edges are still orientated
 }
 
+void U_prime(Cube* cube_state)
+{
+    cubie* temp = malloc(sizeof(cubie));
+
+    temp = &cube_state->corners[0];
+    cube_state->corners[0]=cube_state->corners[3];
+    cube_state->corners[3]=cube_state->corners[7];
+    cube_state->corners[7]=cube_state->corners[4];
+    cube_state->corners[4]=*temp;
+
+    // corner orientation has not changed
+
+    temp = &cube_state->edges[0];
+    cube_state->edges[0]=cube_state->edges[7];
+    cube_state->edges[7]=cube_state->edges[8];
+    cube_state->edges[8]=cube_state->edges[4];
+    cube_state->edges[4]=*temp;
+    free(temp);
+}
+
 void D(Cube* cube_state)
 {
     cubie* temp = malloc(sizeof(cubie));
@@ -196,6 +319,27 @@ void D(Cube* cube_state)
     cube_state->edges[5]=*temp;
     free(temp);
 }
+
+void D_prime(Cube* cube_state)
+{
+    cubie* temp = malloc(sizeof(cubie));
+
+    temp = &cube_state->corners[2];
+    cube_state->corners[2]=cube_state->corners[1];
+    cube_state->corners[1]=cube_state->corners[5];
+    cube_state->corners[5]=cube_state->corners[6];
+    cube_state->corners[6]=*temp;
+    
+    // corners not changed
+
+    temp = &cube_state->edges[2];
+    cube_state->edges[2]=cube_state->edges[5];
+    cube_state->edges[5]=cube_state->edges[10];
+    cube_state->edges[10]=cube_state->edges[6];
+    cube_state->edges[6]=*temp;
+    free(temp);
+}
+
 
 int main()
 {
