ecc33c297aad83b15aee3c9acd56be8c4a938176
Shil16 <shilkpatel2004@gmail.com>
2023-11-03 00:07:37 +0000

    started working on tree for search

--- tree.c
+++ tree.c
@@ -1,12 +1,29 @@
 #include "Cube.h"
 
 #include <stdio.h>
+#include <stdlib.h>
 
-typedef struct treenode
+struct treenode
 {
     Cube node;
+    int number_of_children;
     struct treenode* children;
 };
+typedef struct treenode treenode;
 
 // pointer to malloc of children
 // to add, malloc again then memcopy
+
+treenode* create_treenode(Cube data,int number_of_children)
+{
+    treenode* node;
+    node->node=data;
+    treenode* tree_child = malloc(sizeof(treenode)*number_of_children);
+    return node;
+}
+
+void add_child(treenode* node,treenode child,int index)
+{
+    *(node->children+index)=child;
+}
+
